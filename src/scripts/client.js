b4b58af9e865f10fa97645662a9ff60ea40c5310
the-sides <sides798@gmail.com>
2020-04-14 22:01:30 -0400

    offers and more game logic

--- src/scripts/client.js
+++ src/scripts/client.js
@@ -1,10 +1,27 @@
 const checkStage = document.querySelector(".checks");
 const caseStage = document.querySelector(".cases");
+const offerStage = document.querySelector(".banker input");
 const cash = [0.01, 1, 5, 10, 100, 1000, 10000, 100000, 500000, 1000000];
 let gameState = {
   newGame: true,
   offer: 0,
 };
+
+const makeOffer = () => {
+  let sum = 0;
+  let total = 0;
+  const check = checkStage.querySelectorAll("input");
+  check.forEach((elm) => {
+    if (elm.checked == false) {
+      sum += Number(elm.dataset.cash);
+      total += 1;
+    }
+  });
+  console.table(sum, total)
+  offerStage.value = ((sum / total) * 0.9).toFixed(2);
+};
+
+window.makeOffer = makeOffer;
 
 const findAndCheck = (val) => {
   const checks = checkStage.querySelectorAll(".check");
@@ -29,33 +46,36 @@ const handleOpen = (ev) => {
   const cash = ev.srcElement.dataset.cash;
   ev.srcElement.textContent = cash;
   findAndCheck(cash);
+  makeOffer();
 };
 
-const generateCase = (stage, caseNum, value) => {
+const generateCase = (stage, caseNum, value, disable = false) => {
   const caseElm = document.createElement("button");
   caseElm.textContent = caseNum;
   caseElm.dataset.cash = value;
   caseElm.classList.add("case");
   caseElm.classList.add("btn");
+  if (disable) caseElm.disabled = true;
   caseElm.addEventListener("mouseup", handleOpen);
   stage.appendChild(caseElm);
 };
 
-const generateCases = (cash) => {
-  const cashArr = [...cash]
+const generateCases = (cash, disable = false) => {
+  const cashArr = [...cash];
 
   while (cashArr.length > 0) {
     generateCase(
       caseStage,
       11 - cashArr.length,
-      cashArr.splice(Math.round(Math.random() * (cashArr.length - 1)), 1)
+      cashArr.splice(Math.round(Math.random() * (cashArr.length - 1)), 1),
+      disable
     );
   }
-}
+};
 
 const generateChecks = (cashVal) => {
-  const checkRawHTML = `<span class="check" data-cash="${cashVal}">
-  <input type="checkbox" readonly onclick="return false;">
+  const checkRawHTML = `<span class="check" data-cash=${cashVal}>
+  <input type="checkbox" readonly onclick="return false;" data-cash=${cashVal}>
   <p>$${cashVal}</p>
 </span>`;
   const checkElm = document.createElement("div");
@@ -63,19 +83,17 @@ const generateChecks = (cashVal) => {
   checkStage.appendChild(checkElm);
 };
 
-// const revealCase = (caseNum)
-
 cash.forEach(generateChecks);
 
-generateCases(cash)
+generateCases(cash, true);
 
 const newGame = () => {
   gameState.newGame = true;
-  caseStage.innerHTML = '';
-  checkStage.innerHTML = '';
-  console.log(cash)
-  generateCases(cash)
+  caseStage.innerHTML = "";
+  checkStage.innerHTML = "";
+  console.log(cash);
+  generateCases(cash);
   cash.forEach(generateChecks);
 };
 
-document.querySelector('.start').addEventListener('mouseup', newGame)
+document.querySelector(".start").addEventListener("mouseup", newGame);
