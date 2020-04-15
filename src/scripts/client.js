e52a2e9aa358dde37da8f40d74ce5aac339ecb5b
the-sides <sides798@gmail.com>
2020-04-14 20:12:43 -0400

    base game before functionality and some styles

--- src/scripts/client.js
+++ src/scripts/client.js
@@ -1,1 +1,33 @@
-console.log('client side script. reporting for duty')
+const checkStage = document.querySelector('.checks')
+const caseStage = document.querySelector(".cases");
+const cash = [0.01, 1, 5, 10, 100, 1000, 10000, 100000, 500000, 1000000];
+
+const generateCase = (stage, caseNum, value) => {
+  const caseElm = document.createElement("button");
+  caseElm.textContent = caseNum;
+  caseElm.dataset.cash = value;
+  caseElm.classList.add("case");
+  stage.appendChild(caseElm);
+};
+
+const generateChecks = cashVal => {
+  const checkRawHTML = `<span class="check">
+  <input type="checkbox" readonly onclick="return false;">
+  <p>$${cashVal}</p>
+</span>`
+  const checkElm = document.createElement('div')
+  checkElm.innerHTML = checkRawHTML
+  checkStage.appendChild(checkElm)
+}
+
+// const revealCase = (caseNum)
+
+cash.forEach(generateChecks)
+
+while (cash.length > 0) {
+  generateCase(
+    caseStage,
+    11 - cash.length,
+    cash.splice(Math.round(Math.random() * (cash.length - 1)), 1)
+  );
+}
