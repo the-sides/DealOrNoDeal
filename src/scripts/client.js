4cb8cdc0f5ae956b48bc632f722d9eeb76331273
the-sides <sides798@gmail.com>
2020-04-14 21:17:57 -0400

    game routine except for offer

--- src/scripts/client.js
+++ src/scripts/client.js
@@ -1,17 +1,30 @@
 const checkStage = document.querySelector(".checks");
 const caseStage = document.querySelector(".cases");
 const cash = [0.01, 1, 5, 10, 100, 1000, 10000, 100000, 500000, 1000000];
+let gameState = {
+  newGame: true,
+  offer: 0,
+};
 
 const findAndCheck = (val) => {
   const checks = checkStage.querySelectorAll(".check");
   checks.forEach((elm) => {
-    if ((elm.dataset.cash === val)) {
+    if (elm.dataset.cash === val) {
       elm.children[0].checked = true;
     }
   });
 };
 
 const handleOpen = (ev) => {
+  if (gameState.newGame) {
+    gameState.newGame = false;
+    ev.srcElement.classList.add("picked");
+    return;
+  }
+  if (ev.srcElement.classList.contains("picked")) {
+    return;
+  }
+
   ev.srcElement.classList.add("opened");
   const cash = ev.srcElement.dataset.cash;
   ev.srcElement.textContent = cash;
@@ -28,6 +41,18 @@ const generateCase = (stage, caseNum, value) => {
   stage.appendChild(caseElm);
 };
 
+const generateCases = (cash) => {
+  const cashArr = [...cash]
+
+  while (cashArr.length > 0) {
+    generateCase(
+      caseStage,
+      11 - cashArr.length,
+      cashArr.splice(Math.round(Math.random() * (cashArr.length - 1)), 1)
+    );
+  }
+}
+
 const generateChecks = (cashVal) => {
   const checkRawHTML = `<span class="check" data-cash="${cashVal}">
   <input type="checkbox" readonly onclick="return false;">
@@ -42,10 +67,15 @@ const generateChecks = (cashVal) => {
 
 cash.forEach(generateChecks);
 
-while (cash.length > 0) {
-  generateCase(
-    caseStage,
-    11 - cash.length,
-    cash.splice(Math.round(Math.random() * (cash.length - 1)), 1)
-  );
-}
+generateCases(cash)
+
+const newGame = () => {
+  gameState.newGame = true;
+  caseStage.innerHTML = '';
+  checkStage.innerHTML = '';
+  console.log(cash)
+  generateCases(cash)
+  cash.forEach(generateChecks);
+};
+
+document.querySelector('.start').addEventListener('mouseup', newGame)
